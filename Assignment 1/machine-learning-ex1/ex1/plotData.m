f882216eba0f653a3c80631fa7dd62634284ad02
Soumajit Mullick <soumajitmullick68@gmail.com>
2019-10-22 20:46:32 +0530

    Assignment 1

---
+++ Assignment 1/machine-learning-ex1/ex1/plotData.m
@@ -0,0 +1,16 @@
+function plotData(x, y)
+%PLOTDATA Plots the data points x and y into a new figure 
+%   PLOTDATA(x,y) plots the data points and gives the figure axes labels of
+%   population and profit.
+
+figure; % open a new figure window
+plot(x, y, "rx", "MarkerSize", 10);
+xlabel("Population"); % setting the x label as population
+ylabel("Profit");            % setting the y label
+
+
+
+
+% ============================================================
+
+end
